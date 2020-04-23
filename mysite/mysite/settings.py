8d10ede0599e01e2f3c78ce96e73428af1059b73
zongwu <yangzong5@gmail.com>
2020-04-23 16:00:29 +0800

    initial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#tc+knnxd#!3qjffyi6pp$_8r)egho$)n%=xbp=^h9g5qz3en4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
