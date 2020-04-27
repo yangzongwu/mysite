021f0771c80520f8901f08bbc1d672c17537082c
zongwu <yangzong5@gmail.com>
2020-04-27 14:14:25 +0800

    initial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,8 +83,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
